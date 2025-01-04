9d31562161b96e856adc672276df6851c94eee77
alex volkov <admin@gmail.com>
2025-01-04 16:22:44 +0200

    Добавлены исправления для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/crypto-converter/', // Замени на название твоего репозитория
+  base: '/crypto-convector-vue/', // Замени на название твоего репозитория
   plugins: [
     vue(),
     vueDevTools(),
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-const {defineConfig} = require('@vue/cli-service')
-  module.exports = defineConfig({
-    publicPath: '/crypto-convector-vue/',
-    transpileDependencies: true
-  })
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = {
+  publicPath: '/crypto-convector-vue/',  // Путь к твоему репозиторию
+  transpileDependencies: true,
+}
